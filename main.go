8c8473c2f380435ec25d43ea5ad16572279c7bde
Curtis La Graff <curtis@lagraff.me>
2019-04-11 19:38:32 -0400

    gitignore debug file; create basic server

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+const port = ":8080"
+
+func serveRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Helo world")
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Printf("Server listening on port: %s\n", port)
+
+	http.HandleFunc("/", serveRequest)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		panic(err)
+	}
 }
